934dcf459d9905904214185dc9c9fa148ee636a0
karabolamola <lamola1@msn.com>
2024-05-27 11:21:45 +0200

    Changes

---
+++ ServerAndClientApplications.Api.Server/Controllers/ValuesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerAndClientApplications.Api.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    
+}
