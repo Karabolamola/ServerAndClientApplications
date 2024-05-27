89121efb9108fe7370e6335e131f0b13e731563e
karabolamola <lamola1@msn.com>
2024-05-27 11:30:56 +0200

    Changes

--- ServerAndClientApplications.Api.Server/Controllers/ValuesController.cs
+++ ServerAndClientApplications.Api.Server/Controllers/ValuesController.cs
@@ -6,5 +6,23 @@ namespace ServerAndClientApplications.Api.Server.Controllers;
 [ApiController]
 public class ValuesController : ControllerBase
 {
-    
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var output = new Person
+        {
+            Id = 1,
+            FirstName = "John",
+            LastName = "Doe"
+        };
+        
+        return Ok(output);
+    }
+}
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 }
