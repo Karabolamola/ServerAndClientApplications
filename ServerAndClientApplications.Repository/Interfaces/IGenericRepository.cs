cf29c98fea5c4d4e869fc3addcdb82481c86bb30
karabolamola <lamola1@msn.com>
2024-05-28 09:39:30 +0200

    Changes

--- ServerAndClientApplications.Repository/Interfaces/IGenericRepository.cs
+++ ServerAndClientApplications.Repository/Interfaces/IGenericRepository.cs
@@ -1,6 +1,11 @@
 namespace ServerAndClientApplications.Repository.Interfaces;
 
-public interface IGenericRepository
+public interface IGenericRepository<T>
 {
-    
+    Task<List<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(int id);
+    Task InsertAsync(T model);
+    Task UpdateAsync(T model);
+    Task DeleteAsync(T model);
+    Task SaveAsync();
 }
--- ServerAndClientApplications.Repository/Repos/GenericRepository.cs
+++ ServerAndClientApplications.Repository/Repos/GenericRepository.cs
@@ -2,7 +2,40 @@ using ServerAndClientApplications.Repository.Interfaces;
 
 namespace ServerAndClientApplications.Repository.Repos;
 
-public class GenericRepository : IGenericRepository
+public class GenericRepository<T> : IGenericRepository
 {
-    
+    private readonly GenericApplicationDbContext<T> _context = new GenericApplicationDbContext<T>();
+    public async Task<List<T>> GetAllAsync()
+    {
+        return await _context.DbSets.ToListAsync();
+        // return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await _context.DbSets.SingleAsync(x => x.Id == id);
+        // return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task InsertAsync(T model)
+    {
+        await _context.DbSets.AddAsync(model);
+    }
+
+    public Task UpdateAsync(T model)
+    {
+        _context.DbSets.Update(model);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(T model)
+    {
+        _context.DbSets.Remove(model);
+        return Task.CompletedTask;
+    }
+
+    public async Task SaveAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
 }
