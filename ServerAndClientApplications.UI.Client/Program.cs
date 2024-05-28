e46398a5d3c41b152424ae3e640ac2a33eec8e8d
karabolamola <lamola1@msn.com>
2024-05-28 09:31:13 +0200

    Changes

--- ServerAndClientApplications.UI.Client/Program.cs
+++ ServerAndClientApplications.UI.Client/Program.cs
@@ -1,14 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-namespace ServerAndClientApplications.Console.Client;
+namespace ServerAndClientApplications.UI.Client;
 
 class Program
 {
     static async Task Main(string[] args)
     {
         
-        System.Console.WriteLine("Press any key to continue...");
-        System.Console.ReadLine();
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadLine();
         using (HttpClient client = new HttpClient())
         {
             var response = await client.GetAsync("http://localhost:5255/api/values");
@@ -16,13 +16,13 @@ class Program
             if (response.IsSuccessStatusCode)
             {
                 string message = await response.Content.ReadAsStringAsync();
-                System.Console.WriteLine(message);
+                Console.WriteLine(message);
             }
             else
             {
-                System.Console.WriteLine($"Error code: {response.StatusCode}");
+                Console.WriteLine($"Error code: {response.StatusCode}");
             }
         }
-        System.Console.ReadLine();
+        Console.ReadLine();
     }
 }
