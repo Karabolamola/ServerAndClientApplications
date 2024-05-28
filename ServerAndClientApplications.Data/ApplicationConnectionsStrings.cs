f3e48ccf635efe04843d45972b2e19dbd6ebe5bc
karabolamola <lamola1@msn.com>
2024-05-28 11:44:32 +0200

    Changes

--- ServerAndClientApplications.Data/ApplicationConnectionsStrings.cs
+++ ServerAndClientApplications.Data/ApplicationConnectionsStrings.cs
@@ -1,7 +1,25 @@
 namespace ServerAndClientApplications.Data;
 
-public static class ApplicationConnectionsStrings
+public class ApplicationConnectionsStrings
 {
+    private static DatabaseSettingsModel _databaseSettingsModel;
+
+    public ApplicationConnectionsStrings(DatabaseSettingsModel databaseSettingsModel)
+    {
+        _databaseSettingsModel = databaseSettingsModel;
+    }
+    
+    public void GetDatabase(string databaseName)
+    {
+        _databaseSettingsModel.DatabaseName = databaseName;
+    }
+
+    public void GetServerNameAndPort(string serverName, int port)
+    {
+        _databaseSettingsModel.ServerName = serverName;
+        _databaseSettingsModel.Port = port;
+    }
+    
     public static string MongoAtlas()
     {
         return "mongodb+srv://karabolamola:Niceg%2388@karabodeveloperdb.ogw6w1y.mongodb.net/?retryWrites=true&w=majority";
@@ -9,6 +27,14 @@ public static class ApplicationConnectionsStrings
 
     public static string SqlLocal()
     {
-        return "Server=localhost,1433;Database=developer_db;User=sa;Password=Niceg#88;TrustServerCertificate=true;";
+        // Default string = "Server=localhost,1433;Database=developer_db;User=sa;Password=Niceg#88;TrustServerCertificate=true;";
+        return $"Server={_databaseSettingsModel.ServerName},{_databaseSettingsModel.Port};Database={_databaseSettingsModel.DatabaseName};User=sa;Password=Niceg#88;TrustServerCertificate=true;";
     }
+}
+
+public class DatabaseSettingsModel
+{
+    public string ServerName { get; set; }
+    public int Port { get; set; }
+    public string DatabaseName { get; set; }
 }
